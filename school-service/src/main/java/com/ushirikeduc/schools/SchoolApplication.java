f0968ede2543cd29879e1c432edbc169c6a85679
AHADI5 <gloireahadi9@gmail.com>
2024-03-05 18:07:11 +0100

    Setting Up the project with the school service

---
+++ school-service/src/main/java/com/ushirikeduc/schools/SchoolApplication.java
@@ -0,0 +1,14 @@
+package com.ushirikeduc.schools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class SchoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class);
+
+    }
+}
